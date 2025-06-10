1f5846b3be1c772482f3e79849d65de73fadb4a9
Vaidya Raviteja <vydyatej@gmail.com>
2025-06-11 02:08:09 +0530

    changes made in portfolio

--- day1/task/src/Projects.jsx
+++ day1/task/src/Projects.jsx
@@ -14,6 +14,26 @@ function Projects(){
             <p>Project - 3</p>
             <a href='https://vaidyaraviteja.github.io/Web-Dev-Tasks/CSS%20-%20TASKS/Day%2011/workshop.html' target='_blank' rel='noopener noreferrer'>Live Demo</a>
         </div>
+         <div id='p4'>
+            <p>Project - 4</p>
+            <a href='https://react-81p1-git-main-vaidyaravitejas-projects.vercel.app' target='_blank' rel='noopener noreferrer'>Live Demo</a>
+        </div>
+        <div id='p5'>
+            <p>Project - 5</p>
+            <a href='https://brarchitects-git-main-vaidyaravitejas-projects.vercel.app' target='_blank' rel='noopener noreferrer'>Live Demo</a>
+        </div>
+        <div id='p6'>
+            <p>Project - 6</p>
+            <a href='https://react-7maf-git-main-vaidyaravitejas-projects.vercel.app' target='_blank' rel='noopener noreferrer'>Live Demo</a>
+        </div>
+        <div id='p7'>
+            <p>Project - 7</p>
+            <a href='https://classbasedcomponentsexamples-git-main-vaidyaravitejas-projects.vercel.app'>Live Demo</a>
+        </div>
+        <div id='p8'>
+            <p>Project - 8</p>
+            <a href='https://react-ieir-git-main-vaidyaravitejas-projects.vercel.app'>Live Demo</a>
+        </div>
     </div>
 
 }
